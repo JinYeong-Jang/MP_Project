0d593e58dd881c64198b410baf6c5c67953bbe31
hojin3777 <hojin3777@gmail.com>
2024-05-20 18:57:49 +0900

    load 방식2 수정, README 업데이트

--- convert_keil_m2.c
+++ convert_keil_m2.c
@@ -7,42 +7,50 @@
 #define BUFFER_SIZE 16
 #define HEADER_SIZE 54 //bytes
 #define IMAGE_SIZE 960*160*4 //bytes
+#define COLOR_SIZE 960*160 //bytes
 
 int main() {
     uint8_t *p;
     uint8_t *rgba; //uint8_t rgba[IMAGE_SIZE]; //RGBA array
     uint8_t *rgb; //uint8_t rgb[IMAGE_SIZE * 3/4]; //RGB array
-    uint8_t *rgbcomp; //uint8_t rgbcomp[IMAGE_SIZE / 4]; //compressed RGB
-    p = 0x40000000; //memory pointer - size 4000byte
+    uint32_t *rgbcomp; //uint8_t rgbcomp[IMAGE_SIZE / 4]; //compressed RGB
+    size_t i, j;
+    p = 0x40000000 + HEADER_SIZE; //memory pointer - size 4000byte
+    //fseek(fileIn, HEADER_SIZE-1, SEEK_SET); //set pointer after header
     rgba = 0x40200000;
     rgb = 0x40400000;
     rgbcomp = 0x40600000;
     //0x40200000, 0x407FFFFF Memory Map needed
-    size_t i = 0;
-    size_t j = 0;
-    size_t k = 0;
 
-    p = 0x40000000 + HEADER_SIZE;   //fseek(fileIn, HEADER_SIZE-1, SEEK_SET); //set pointer after header
+
     for(i = 0 ; i<IMAGE_SIZE; i++){
         rgba[i] = p[i];
     } //fread(rgba, sizeof(uint8_t), IMAGE_SIZE, fileIn); //Whole rgba read
     
-    for (k = 0; k < 3; k++){ //R, G, B  *3 times
-        for (i = 0; i < IMAGE_SIZE; i += 4) {
-            rgb[j++] = rgba[i+k];     // RRRGGGBBB order
-        } // Convert RGBA to RGB by ignoring the alpha channel
-    }
+    for (i = 0, j = 0; i < IMAGE_SIZE; i += 4, j++) {
+        rgb[j] = rgba[i];   // Red
+        rgb[j+COLOR_SIZE] = rgba[i + 1]; // Green
+        rgb[j+COLOR_SIZE*2] = rgba[i + 2]; // Blue
+        // Alpha channel rgba[i + 3] is ignored
+    } // Convert RGBA to RGB by ignoring the alpha channel, RRRGGGBBB order
 
-    unsigned char mask = 0b11100000; //R[7:5], G[7:5] masking
+    uint32_t mask = 0xE0E0E0E0; //masking 0b1110_0000 by register length
+    uint32_t *readbuffer;
+    readbuffer = 0x40400000;
     i = 0;
-    for(k = 0; k < 3; k++){
-        j = 0;
-        while (j < IMAGE_SIZE/4) {
-            rgbcomp[j++] += (rgb[i++] & mask) >> k*3;
-        }
+	j = 0;
+    while (j < IMAGE_SIZE/16) {
+        rgbcomp[j++] += readbuffer[i++]&mask;
     }
-    
-
+    j=0;
+    while (j < IMAGE_SIZE/16) {
+        rgbcomp[j++] += (readbuffer[i++]&mask) >> 3;
+    }
+    mask -= 0x20202020; //0xE0E0E0E0 to 0xC0C0C0C0
+    j=0;
+    while (j < IMAGE_SIZE/16) {
+        rgbcomp[j++] += (readbuffer[i++]&mask) >> 6;
+    }
     printf("Output 'output_rgbcomp.bmp' created.\n");
 
     _sys_exit(0);
--- convert_keil_test.c
+++ convert_keil_test.c
@@ -7,31 +7,32 @@
 #define BUFFER_SIZE 16
 #define HEADER_SIZE 54 //bytes
 #define IMAGE_SIZE 960*160*4 //bytes
+#define COLOR_SIZE 960*160 //bytes
 
 int main() {
     uint8_t *p;
     uint8_t *rgba; //uint8_t rgba[IMAGE_SIZE]; //RGBA array
     uint8_t *rgb; //uint8_t rgb[IMAGE_SIZE * 3/4]; //RGB array
     uint32_t *rgbcomp; //uint8_t rgbcomp[IMAGE_SIZE / 4]; //compressed RGB
+    size_t i, j;
     p = 0x40000000 + HEADER_SIZE; //memory pointer - size 4000byte
     //fseek(fileIn, HEADER_SIZE-1, SEEK_SET); //set pointer after header
     rgba = 0x40200000;
     rgb = 0x40400000;
     rgbcomp = 0x40600000;
     //0x40200000, 0x407FFFFF Memory Map needed
-    size_t i = 0;
-    size_t j = 0;
-    size_t k = 0;
+
 
     for(i = 0 ; i<IMAGE_SIZE; i++){
         rgba[i] = p[i];
     } //fread(rgba, sizeof(uint8_t), IMAGE_SIZE, fileIn); //Whole rgba read
     
-    for (k = 0; k < 3; k++){ //R, G, B  *3 times
-        for (i = 0; i < IMAGE_SIZE; i += 4) {
-            rgb[j++] = rgba[i+k];     // RRRGGGBBB order
-        } // Convert RGBA to RGB by ignoring the alpha channel
-    }
+    for (i = 0, j = 0; i < IMAGE_SIZE; i += 4, j++) {
+        rgb[j] = rgba[i];   // Red
+        rgb[j+COLOR_SIZE] = rgba[i + 1]; // Green
+        rgb[j+COLOR_SIZE*2] = rgba[i + 2]; // Blue
+        // Alpha channel rgba[i + 3] is ignored
+    } // Convert RGBA to RGB by ignoring the alpha channel, RRRGGGBBB order
 
     uint32_t mask = 0xE0E0E0E0; //masking 0b1110_0000 by register length
     uint32_t *readbuffer;
@@ -50,17 +51,6 @@ int main() {
     while (j < IMAGE_SIZE/16) {
         rgbcomp[j++] += (readbuffer[i++]&mask) >> 6;
     }
-	/* //위의 3개의 while문과 같은 코드
-	for(k = 0; k < 3; k++){
-        j = 0;
-        while (j < IMAGE_SIZE/16 && k < 2) {
-            rgbcomp[j++] += readbuffer[i++]&mask >> k*3;
-        }
-				while (j < IMAGE_SIZE/16 && k == 2) {
-            rgbcomp[j++] += readbuffer[i++]&bmask >> 6;
-        }
-    }
-	*/
     printf("Output 'output_rgbcomp.bmp' created.\n");
 
     _sys_exit(0);
