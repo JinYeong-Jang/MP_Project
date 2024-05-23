2823dd5911594b135e6a9e6dfb696d6271ba9434
hojin3777 <hojin3777@gmail.com>
2024-05-23 16:26:42 +0900

    decode한 RGB hex파일 메모리 위치 0x5000--으로 변경(0x4000--은 main과 겹침) stb_image 라이브러리 더이상 사용하지 않아 폴더 이름 변경

---
+++ convert_keil_realRGB.c
@@ -0,0 +1,49 @@
+//last-update 0523-1630
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define IMAGE_SIZE 960*640*4 //bytes, 0x25_8000
+#define COLOR_SIZE 960*640 //bytes
+
+int main() {
+    uint8_t *rgba; //uint8_t rgba[IMAGE_SIZE]; //RGBA array
+    uint8_t *rgb; //uint8_t rgb[IMAGE_SIZE * 3/4]; //RGB array
+    uint32_t *rgbcomp; //uint8_t rgbcomp[IMAGE_SIZE / 4]; //compressed RGB
+    uint32_t *readbuffer; //same as rgb[] but access in 32bit
+    size_t i, j;
+    rgba = 0x50000000;
+    rgb = rgba+IMAGE_SIZE;
+    rgbcomp = rgb+IMAGE_SIZE;
+    readbuffer = rgba+IMAGE_SIZE;
+    //0x40000000, 0x40708000 Memory Map needed
+    
+    for (i = 0, j = 0; i < IMAGE_SIZE; i += 4, j++) {
+        rgb[j] = rgba[i];   // Red
+        rgb[j+COLOR_SIZE] = rgba[i + 1]; // Green
+        rgb[j+COLOR_SIZE*2] = rgba[i + 2]; // Blue
+        // Alpha channel rgba[i + 3] is ignored
+    } // Convert RGBA to RGB by ignoring the alpha channel, RRRGGGBBB order
+
+    uint32_t mask = 0xE0E0E0E0; //masking 0b1110_0000 by register length
+
+    i = 0;
+	j = 0;
+    while (j < IMAGE_SIZE/16) {
+        rgbcomp[j++] += readbuffer[i++]&mask;
+    }
+    j=0;
+    while (j < IMAGE_SIZE/16) {
+        rgbcomp[j++] += (readbuffer[i++]&mask) >> 3;
+    }
+    mask -= 0x20202020; //0xE0E0E0E0 to 0xC0C0C0C0
+    j=0;
+    while (j < IMAGE_SIZE/16) {
+        rgbcomp[j++] += (readbuffer[i++]&mask) >> 6;
+    }
+
+    printf("Convertion done.\n");
+
+    _sys_exit(0);
+}
--- convert_keil_test.c
+++ convert_keil_test.c
@@ -1,13 +1,11 @@
-//last-update 0508-2240
+//last-update 0520-2240
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 16
-#define HEADER_SIZE 54 //bytes
-#define IMAGE_SIZE 960*160*4 //bytes
-#define COLOR_SIZE 960*160 //bytes
+#define IMAGE_SIZE 960*640*4 //bytes, 0x25_8000
+#define COLOR_SIZE 960*640 //bytes
 
 int main() {
     uint8_t *p;
@@ -15,13 +13,14 @@ int main() {
     uint8_t *rgb; //uint8_t rgb[IMAGE_SIZE * 3/4]; //RGB array
     uint32_t *rgbcomp; //uint8_t rgbcomp[IMAGE_SIZE / 4]; //compressed RGB
     size_t i, j;
-    p = 0x40000000 + HEADER_SIZE; //memory pointer - size 4000byte
-    //fseek(fileIn, HEADER_SIZE-1, SEEK_SET); //set pointer after header
-    rgba = 0x40200000;
-    rgb = 0x40400000;
-    rgbcomp = 0x40600000;
+    uint32_t *readbuffer; //same as rgb[] but access in 32bit
+    //p = 0x40000000 + HEADER_SIZE; //memory pointer - size 4000byte
+	p = 0x40000000;
+    rgba = p+IMAGE_SIZE;
+    rgb = rgba+IMAGE_SIZE;
+    rgbcomp = rgb+IMAGE_SIZE;
+    readbuffer = rgba+IMAGE_SIZE;
     //0x40200000, 0x407FFFFF Memory Map needed
-
 
     for(i = 0 ; i<IMAGE_SIZE; i++){
         rgba[i] = p[i];
@@ -35,8 +34,7 @@ int main() {
     } // Convert RGBA to RGB by ignoring the alpha channel, RRRGGGBBB order
 
     uint32_t mask = 0xE0E0E0E0; //masking 0b1110_0000 by register length
-    uint32_t *readbuffer;
-    readbuffer = 0x40400000;
+
     i = 0;
 	j = 0;
     while (j < IMAGE_SIZE/16) {
