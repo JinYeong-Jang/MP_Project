96ba82c934bb76cf5edee15de7ce62cef0c312e7
gksqls1103 <gksqls1103@naver.com>
2024-05-27 22:22:30 +0900

    Update convert_gray1a.c

--- convert_gray1a.c
+++ convert_gray1a.c
@@ -10,29 +10,37 @@
 int main() {
     uint8_t* p;
     uint8_t* rgba; //uint8_t rgba[IMAGE_SIZE]; //RGBA array
+	  uint8_t *rgb;
     uint8_t* gray; //uint8_t gray[IMAGE_SIZE / 4]; //Grayscale array
     size_t i, j;
     p = (uint8_t*)0x40000000 + HEADER_SIZE; //memory pointer
     rgba = (uint8_t*)0x40200000;
-    gray = (uint8_t*)0x40400000;
+	  rgb = 0x40400000;
+    gray = (uint8_t*)0x40600000;
     //0x40200000, 0x407FFFFF Memory Map needed
 
-    // RGBA µ¥ÀÌÅÍ¸¦ ÀĞ¾î¿É´Ï´Ù.
+    // RGBA ÂµÂ¥Ã€ÃŒÃ…ÃÂ¸Â¦ Ã€ÃÂ¾Ã®Â¿Ã‰Â´ÃÂ´Ã™.
     for (i = 0; i < IMAGE_SIZE; i++) {
         rgba[i] = p[i];
-    } // ¿¹½Ã¿¡¼­´Â ÆÄÀÏ ÀÔÃâ·Â ´ë½Å ¸Ş¸ğ¸®¿¡¼­ Á÷Á¢ ÀĞ¾î¿É´Ï´Ù.
+    } // Â¿Â¹Â½ÃƒÂ¿Â¡Â¼Â­Â´Ã‚ Ã†Ã„Ã€Ã Ã€Ã”ÃƒÃ¢Â·Ã‚ Â´Ã«Â½Ã… Â¸ÃÂ¸Ã°Â¸Â®Â¿Â¡Â¼Â­ ÃÃ·ÃÂ¢ Ã€ÃÂ¾Ã®Â¿Ã‰Â´ÃÂ´Ã™.
 
-    // RGBA¸¦ ±×·¹ÀÌ½ºÄÉÀÏ·Î º¯È¯ÇÕ´Ï´Ù.
-    for (i = 0, j = 0; i < IMAGE_SIZE; i += 4) {
-        uint8_t r = rgba[i];     // Red
-        uint8_t g = rgba[i + 1]; // Green
-        uint8_t b = rgba[i + 2]; // Blue
-        // ±×·¹ÀÌ½ºÄÉÀÏ °ªÀº RGB °ªÀÇ Æò±ÕÀ¸·Î °è»êµË´Ï´Ù.
-        gray[j++] = (r + g + b) / 3;
+    
+   for (i = 0, j = 0; i < IMAGE_SIZE; i += 4) {
+        rgb[j++] = rgba[i];     // Red
+        rgb[j++] = rgba[i + 1]; // Green
+        rgb[j++] = rgba[i + 2]; // Blue
+        // Alpha channel rgba[i + 3] is ignored
+    } // Convert RGBA to RGB by ignoring the alpha channel
+
+		for (i = 0, j = 0; i < IMAGE_SIZE; i += 4) {
+        uint8_t r = rgb[i];     // Red
+        uint8_t g = rgb[i + 1]; // Green
+        uint8_t b = rgb[i + 2]; // Blue
+        
+        gray[j++] = (r + g + b) /3;
     }
-
     printf("Output 'output_gray.bmp' created.\n");
 
-    // ÇÁ·Î±×·¥ Á¾·á
+    // Ã‡ÃÂ·ÃÂ±Ã—Â·Â¥ ÃÂ¾Â·Ã¡
     _sys_exit(0);
 }
